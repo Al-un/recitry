ccb730a8c8ee202e013859cc65c182b0c3e722c5
Al-un <alun.sng@gmail.com>
2023-03-21 17:19:30 +0900

    Remove console.log

--- ressaite-api/src/core/controllers/MiscController.ts
+++ ressaite-api/src/core/controllers/MiscController.ts
@@ -1,6 +1,5 @@
 import { RequestHandler } from "express";
 
 export const healthCheck: RequestHandler = (_, resp) => {
-  console.log("HEALTH");
   return resp.status(200).json({ status: "All good!" });
 };
